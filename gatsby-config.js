adfe0ece3fb877acad455134cbda4fd591e41c39
jasonbarry <jasbarry@me.com>
2019-05-25 18:07:03 -0700

    Service worker no longer interferes with SSR

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,19 +53,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/blog`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
@@ -76,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-source-stripe`,
       options: {
-        objects: ['Plan', 'Product', 'Sku'],
+        objects: ['Plan', 'Product'],
         secretKey: process.env.STRIPE_SECRET_KEY,
         downloadFiles: false,
         auth: false,
