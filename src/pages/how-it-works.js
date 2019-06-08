14734ec72e635ba8748649aaa6e1c78c8f61478f
jasonbarry <jasbarry@me.com>
2019-06-08 15:35:07 -0700

    Adding YouTube video to How It Works page

--- src/pages/how-it-works.js
+++ src/pages/how-it-works.js
@@ -66,10 +66,32 @@ export default function HowItWorks(props) {
               image={image}
               handleArrival={() => increment(i - 1)}
               handleDeparture={() => decrement(i)}
+              last={i === elaborations.length - 1}
             />
           ))}
         </Column>
       </Flex>
+      <Heading marginTop={240} size={500} textAlign="center">
+        Video walkthrough
+      </Heading>
+      <Heading h={2} marginTop={0} opacity={0.4} size={360} textAlign="center">
+        See FeaturePeek in action.
+      </Heading>
+      <center>
+        <iframe
+          allow="autoplay; fullscreen"
+          allowFullScreen
+          frameBorder="0"
+          height="360"
+          src="https://www.youtube.com/embed/14UwLG1jQwU"
+          style={{
+            boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1), -10px 20px 40px rgba(0, 0, 0, 0.1)',
+            margin: '40px 0 240px',
+          }}
+          title="FeaturePeek product walkthrough"
+          width="640"
+        />
+      </center>
       <Faqs heading="Product FAQs" faqs={productFAQs} />
     </Layout>
   )
