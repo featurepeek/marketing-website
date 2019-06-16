110f7d8dab74a56fccf8029ed4a2d609bce4fd43
jasonbarry <jasbarry@me.com>
2019-06-16 13:55:36 -0700

    Using react-player to defer youtube scripts until video is clicked

--- src/components/VideoWalkthrough.js
+++ src/components/VideoWalkthrough.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react'
 import Box from 'ui-box'
+import ReactPlayer from 'react-player'
 import MediaQuery from 'react-responsive'
 
 import { Heading } from 'primitives'
@@ -19,19 +20,21 @@ export default function VideoWalkthrough() {
             Watch FeaturePeek in action.
           </Heading>
           <center>
-            <iframe
-              allow="autoplay; fullscreen"
-              allowFullScreen
-              frameBorder="0"
-              height="360"
-              src="https://www.youtube.com/embed/14UwLG1jQwU"
-              style={{
-                boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1), -10px 20px 40px rgba(0, 0, 0, 0.1)',
-                margin: '40px 0 160px',
-              }}
-              title="FeaturePeek product walkthrough"
+            <Box
+              boxShadow="0 3px 6px rgba(0, 0, 0, 0.1), -10px 20px 40px rgba(0, 0, 0, 0.1)"
+              marginTop={40}
+              marginBottom={160}
               width={mobile ? '100%' : 640}
-            />
+            >
+              <ReactPlayer
+                controls
+                playing
+                light
+                url="https://www.youtube.com/embed/14UwLG1jQwU"
+                width={mobile ? '100%' : 640}
+                wrapper="center"
+              />
+            </Box>
           </center>
         </Box>
       )}
