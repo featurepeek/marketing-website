83dea7293dca5a7a0f0c384b93d670a66c77b3ea
jasonbarry <jasbarry@me.com>
2019-10-07 16:12:16 -0700

    Adding a story for Button

--- src/stories/button.story.js
+++ src/stories/button.story.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { text, number, select } from '@storybook/addon-knobs'
 
 import { Button, Flex } from 'primitives'
 
 export default { title: 'Button' }
 
-export const withText = () => (
+export const defaultButton = () => (
   <Flex alignItems="center" justifyContent="center" height={100} width="100%">
-    <Button>Button</Button>
+    <Button
+      background={select('Background', ['teal', 'green', 'purple', 'blue', 'green', 'white'])}
+      fontSize={number('Font size', 18)}
+    >
+      {text('Label', 'Button label')}
+    </Button>
   </Flex>
 )
