5c6bf5cae09921534260f8e41599b9d4cc48ac94
jasonbarry <jasbarry@me.com>
2020-04-01 16:32:52 -0700

    Mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -124,18 +124,8 @@ export default function Nav() {
             <Box>
               <UnorderedList marginLeft={76} marginTop={16}>
                 <ListItem>
-                  <Link href="/for-frontend-developers" fontSize={21}>
-                    For Front-end Developers
-                  </Link>
-                </ListItem>
-                <ListItem>
-                  <Link href="/for-ux-designers" fontSize={21}>
-                    For UX Designers
-                  </Link>
-                </ListItem>
-                <ListItem>
-                  <Link href="/for-project-managers" fontSize={21}>
-                    For Project Managers
+                  <Link href="/product" fontSize={21}>
+                    Product
                   </Link>
                 </ListItem>
                 <ListItem>
@@ -146,6 +136,11 @@ export default function Nav() {
                 <ListItem>
                   <Link href="/pricing" fontSize={21}>
                     Pricing
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link href="/blog" fontSize={21}>
+                    Blog
                   </Link>
                 </ListItem>
                 <ListItem>
