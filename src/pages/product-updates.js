e04a0419d14c1bb6fc7db416d9b8727e98672611
jasonbarry <jasbarry@me.com>
2020-04-06 14:17:36 -0700

    Product update page tuneup

--- src/pages/product-updates.js
+++ src/pages/product-updates.js
@@ -6,7 +6,7 @@ import Layout from 'components/Layout'
 import SEO from 'components/Seo'
 import JoinMailingList from 'components/JoinMailingList'
 
-import { Heading, Link, Paragraph } from 'primitives'
+import { Flex, Heading, Icon, Link, Paragraph } from 'primitives'
 
 import { rhythm } from 'utils/typography'
 
@@ -18,36 +18,59 @@ export default function Campaign(props) {
   return (
     <Layout location={props.location} title={siteTitle}>
       <SEO title="All product updates" location={props.location} />
-      <Heading marginTop={0} size={400}>
-        FeaturePeek product updates
+      <Heading marginTop={0} size={500} textAlign="center">
+        FeaturePeek Product Updates
       </Heading>
-      <Paragraph>
+      <Paragraph textAlign="center">
         We send out a product newsletter once or twice a month, informing our users of new features and product
         improvements.
       </Paragraph>
-      <Paragraph>Enter your email address below to get notified as soon as new features become available.</Paragraph>
-      <Box maxWidth={420}>
-        <JoinMailingList />
+      <Paragraph marginBottom={24} textAlign="center">
+        Enter your email address below to get notified as soon as new features become available.
+      </Paragraph>
+      <center>
+        <Box marginBottom={80} maxWidth={320}>
+          <JoinMailingList />
+        </Box>
+      </center>
+      <Box background="#f8f8f8" margin={-39} paddingX={40}>
+        <Flex justifyContent="center">
+          <Box marginBottom={80} maxWidth={880}>
+            <Heading marginTop={40} marginLeft={16} size={300}>
+              Archive
+            </Heading>
+            {campaigns
+              .sort((a, b) => (new Date(a.node.send_time) < new Date(b.node.send_time) ? 1 : -1))
+              .map(({ node }) => {
+                const title = node.settings.title
+                return (
+                  <Box
+                    key={node.campaignId}
+                    background="white"
+                    borderRadius={8}
+                    boxShadow="0 1px 2px rgba(0, 0, 0, 0.1)"
+                    margin={16}
+                    paddingX={16}
+                    paddingY={8}
+                  >
+                    <Flex>
+                      <Icon icon="far fa-newspaper" color="#ccc" fontSize={32} marginRight={16} marginTop={4} />
+                      <Box>
+                        <small>{node.send_time}</small>
+                        <h3 style={{ marginBottom: rhythm(1 / 4), marginTop: rhythm(1 / 4) }}>
+                          <Link href={`/product-updates/${node.campaignId}`}>
+                            {title.replace(/product updates?:/gi, '')}
+                          </Link>
+                        </h3>
+                        <p>{node.settings.preview_text}</p>
+                      </Box>
+                    </Flex>
+                  </Box>
+                )
+              })}
+          </Box>
+        </Flex>
       </Box>
-      <Heading marginTop={40} size={300}>
-        Archive
-      </Heading>
-      {campaigns.map(({ node }) => {
-        const title = node.settings.title
-        return (
-          <div key={node.campaignId}>
-            <h3
-              style={{
-                marginBottom: rhythm(1 / 4),
-              }}
-            >
-              <Link href={`/product-updates/${node.campaignId}`}>{title}</Link>
-            </h3>
-            <small>{node.send_time}</small>
-            <p>{node.settings.preview_text}</p>
-          </div>
-        )
-      })}
     </Layout>
   )
 }
