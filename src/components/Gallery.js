35b3f1b4fff0a2761ee6319633812d48d29ae262
jasonbarry <jasbarry@me.com>
2020-05-12 10:11:00 -0700

    Compress images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,25 +8,25 @@ import { Heading } from 'primitives'
 const images = [
   {
     src: '/img/product/drawer-timeline.png',
-    thumbnail: '/img/product/drawer-timeline.png',
+    thumbnail: '/img/product/thumbs/drawer-timeline.png',
     caption: 'FeaturePeek drawer shows a timeline of comments, views, and more',
     thumbnailCaption: 'Leave comments in the FeaturePeek drawer',
   },
   {
     src: '/img/product/drawer-tickets.png',
-    thumbnail: '/img/product/drawer-tickets.png',
+    thumbnail: '/img/product/thumbs/drawer-tickets.png',
     caption: 'File tickets to third-party integrations like GitHub Issues, Trello, and Clubhouse',
     thumbnailCaption: 'File tickets to third-party integrations',
   },
   {
     src: '/img/product/annotate.png',
-    thumbnail: '/img/product/annotate.png',
+    thumbnail: '/img/product/thumbs/annotate.png',
     caption: 'Capture screenshots and screen recordings, and annotate them',
     thumbnailCaption: 'Capture and annotate screenshots and recordings',
   },
   {
     src: '/img/product/measure.png',
-    thumbnail: '/img/product/measure.png',
+    thumbnail: '/img/product/thumbs/measure.png',
     caption: 'Measure the distance between DOM elements',
     thumbnailCaption: 'Measure the distance between DOM elements',
   },
