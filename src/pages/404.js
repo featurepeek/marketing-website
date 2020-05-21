95da03abf562863a53adc22e047400655e17e92f
Jason Barry <jason@featurepeek.com>
2020-05-20 19:22:23 -0700

    Custom 404 page (#173)
    * Custom 404 page
    
    * 5-7-5

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Box from 'ui-box'
 
 import Layout from 'components/Layout'
 import SEO from 'components/Seo'
+
+import { Flex, Paragraph } from 'primitives'
 
 export default function NotFoundPage(props) {
   const { data } = props
@@ -10,9 +13,24 @@ export default function NotFoundPage(props) {
 
   return (
     <Layout location={props.location} title={siteTitle}>
-      <SEO title="404: Not Found" location={props.location} />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <SEO title="404 Not Found" location={props.location} />
+      {/* <Heading marginBottom={40} marginTop={0} size={500} textAlign="center">404: Not Found</Heading> */}
+      <Flex justifyContent="center" position="relative" top={39}>
+        <video autoPlay muted loop src="/video/404.mp4">
+          <source src="/video/404.mp4" type="video/mp4" />
+        </video>
+        <Box marginBottom={80} marginTop={32} marginX={32}>
+          <Paragraph fontSize={32}>
+            <em>The web page you seek</em>
+          </Paragraph>
+          <Paragraph fontSize={32}>
+            <em>cannot be located, but</em>
+          </Paragraph>
+          <Paragraph fontSize={32}>
+            <em>many more exist.</em>
+          </Paragraph>
+        </Box>
+      </Flex>
     </Layout>
   )
 }
